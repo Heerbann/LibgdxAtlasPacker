d85dde07042ab24807f3999436dfcc8eab107719
Heerbann <info@heerbann.com>
2017-01-14 02:13:53 +0100

    Formating

--- LibgdxAtlasPacker/src/com/heerbann/packer/EntryPoint.java
+++ LibgdxAtlasPacker/src/com/heerbann/packer/EntryPoint.java
@@ -1,3 +1,4 @@
+
 package com.heerbann.packer;
 
 import java.io.File;
@@ -10,143 +11,104 @@ import java.util.Scanner;
 public class EntryPoint {
 
 	public static void main (String[] args) {
-		if(args.length == 0 || args.length > 1) System.exit(1);
-		
-		//
-		
+		if (args.length == 0 || args.length > 1) System.exit(1);
 		ArrayList<File> assets = findAllAssets(args[0]);
-		
 		ArrayList<String> cache = new ArrayList<String>();
 		ArrayList<String> filesIndex = new ArrayList<String>();
-		
-		for(int i = 0; i < assets.size(); i++){
-			//System.out.println(assets.get(i));
+		for (int i = 0; i < assets.size(); i++) {
+			// System.out.println(assets.get(i));
 			convert(i, assets.get(i), cache, filesIndex);
 		}
-		
 		write(args[0].concat("_packed.txt"), cache, filesIndex);
 	}
-	
-	private static void write(String output, ArrayList<String> cache, ArrayList<String> filesIndex){
+
+	private static void write (String output, ArrayList<String> cache, ArrayList<String> filesIndex) {
 		PrintWriter writer = null;
 		try {
 			writer = new PrintWriter(output);
-			for(int i = 0 ; i < cache.size(); i++)
-				if(i < cache.size() - 1) writer.print(cache.get(i) + System.lineSeparator());
-				else writer.print(cache.get(i));
+			for (int i = 0; i < cache.size(); i++)
+				if (i < cache.size() - 1)
+					writer.print(cache.get(i) + System.lineSeparator());
+				else
+					writer.print(cache.get(i));
 			writer.close();
-			
 			writer = new PrintWriter(output.concat("_index.txt"));
-			for(int i = 0 ; i < filesIndex.size(); i++)
-				if(i < filesIndex.size() - 1) writer.print(filesIndex.get(i) + System.lineSeparator());
-				else writer.print(filesIndex.get(i));
+			for (int i = 0; i < filesIndex.size(); i++)
+				if (i < filesIndex.size() - 1)
+					writer.print(filesIndex.get(i) + System.lineSeparator());
+				else
+					writer.print(filesIndex.get(i));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} finally {
 			if (writer != null) writer.close();
 		}
 	}
-	
-	private static void convert(int index, File file, ArrayList<String> cache, ArrayList<String> filesIndex){
+
+	private static void convert (int index, File file, ArrayList<String> cache, ArrayList<String> filesIndex) {
 		Scanner scanner = null;
 		try {
-			
 			scanner = new Scanner(file);
-			
 			short x = 0, y = 0, width = 0, height = 0;
 			boolean rotate = false;
 			String name = null;
-			
 			filesIndex.add(scanner.nextLine());
-			
-			for(int i = 1; i < 5; i++)
+			for (int i = 1; i < 5; i++)
 				scanner.nextLine();
-			
 			int k = 0;
-			while(scanner.hasNext()){
+			while (scanner.hasNext()) {
 				String line = scanner.nextLine();
-				
-				switch(k){
+				switch (k) {
 				case 0:
 					name = line;
 					break;
-				case 1: //rotate
+				case 1: // rotate
 					rotate = line.contains("true");
 					break;
-				case 2: //xy
+				case 2: // xy
 					String[] l1 = line.replaceAll("xy: ", "").replaceAll(" ", "").split(",");
 					x = Short.parseShort(l1[0]);
 					y = Short.parseShort(l1[1]);
 					break;
-				case 3: //size
+				case 3: // size
 					String[] l2 = line.replaceAll("size: ", "").replaceAll(" ", "").split(",");
 					width = Short.parseShort(l2[0]);
 					height = Short.parseShort(l2[1]);
 					break;
-				}			
-				
+				}
 				k++;
-				if(k >= 7){
+				if (k >= 7) {
 					k = 0;
 					cache.add(name + "," + pack(x, y, width, height, rotate, index));
-				}			
+				}
 			}
-			
-			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-		}finally{
-			if(scanner != null) scanner.close();
+		} finally {
+			if (scanner != null) scanner.close();
 		}
 	}
-	
-	public static final int maskByte = 0xFF, maskShort = 0xFFFF, maskInt = 0xFFFFFFFF;
-	public static final int shiftByte = 4, shiftShort = 8, shiftInt = 16;
-	
-	//byte [0000 0000]
-	//short [0000 0000] [0000 0000]
-	//int [0000 0000] [0000 0000] [0000 0000] [0000 0000]
-	//long [0000 0000] [0000 0000] [0000 0000] [0000 0000] [0000 0000] [0000 0000] [0000 0000] [0000 0000]
-	
-	//2048 : [0000 1000] [0000 0000]
-	
-	//packed: 1:[0000 000r] 2:[x] 3:[x|y] 4:[y] 5:[w] 6:[w|h] 7:[h] 8:[index]
-	//[0000 000r] [xxxx xxxx] [xxxx yyyy] [yyyy yyyy] [wwww wwww] [wwww hhhh] [hhhh hhhh] [iiii iiii]
-	
-	//0000_0001_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
-	//0000_0000_1111_1111_1111_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
-	//0000_0000_0000_0000_0000_1111_1111_1111_0000_0000_0000_0000_0000_0000_0000_0000;
-	//0000_0000_0000_0000_0000_0000_0000_0000_1111_1111_1111_0000_0000_0000_0000_0000;
-	//0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_1111_1111_1111_0000_0000;
-	//0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_1111_1111;
-	//0000_0001_0000_0000_0001_0000_0000_0001_0000_0001_1110_0000_0001_0000_0000_0000
-	
-	//0000_0001_0000_0000_0001_0000_0000_0001_0000_0001_1110_0000_0001_0000_0000_0000
-	//0001_1110_0000_0001_0000_0000_0000
-	
-	//72075190550401024
-	
-	private static long pack(short x, short y, short width, short height, boolean rotate, int index){
-		return ((((((((((long)(((rotate ? 0x1 : 0x0) & 0x1) << 12) | (x & 0xFFF)) << 12) | (y & 0xFFF)) << 12) | (width & 0xFFF)) << 12) | (height & 0xFFF)) << 8) | index & 0xFF);     
+
+	// [0000 000r] [xxxx xxxx] [xxxx yyyy] [yyyy yyyy] [wwww wwww] [wwww hhhh] [hhhh hhhh] [iiii iiii]
+	private static long pack (short x, short y, short width, short height, boolean rotate, int index) {
+		return ((((((((((long)(((rotate ? 0x1 : 0x0) & 0x1) << 12) | (x & 0xFFF)) << 12) | (y & 0xFFF)) << 12)
+			| (width & 0xFFF)) << 12) | (height & 0xFFF)) << 8) | index & 0xFF);
 	}
-	
+
 	/** finds all files with .atlas in a given folder and its subfolders */
-	private static ArrayList<File> findAllAssets(String pathToParent){
+	private static ArrayList<File> findAllAssets (String pathToParent) {
 		ArrayList<File> assets = new ArrayList<File>();
 		LinkedList<File> toLoad = new LinkedList<File>();
-		
 		File[] files = new File(pathToParent).listFiles();
-		for(int i = 0; i < files.length; i++)
+		for (int i = 0; i < files.length; i++)
 			toLoad.addLast(files[i]);
-		
-		while(!toLoad.isEmpty()){		
+		while (!toLoad.isEmpty()) {
 			File f = toLoad.removeFirst();
-			if(f.isDirectory()){
+			if (f.isDirectory()) {
 				File[] newFiles = f.listFiles();
-				for(int i = 0; i < newFiles.length; i++){
+				for (int i = 0; i < newFiles.length; i++)
 					toLoad.addLast(newFiles[i]);
-				}
-			}else if(f.getName().contains(".atlas")) assets.add(f);
+			} else if (f.getName().contains(".atlas")) assets.add(f);
 		}
 		return assets;
 	}
